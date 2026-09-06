9d3d2e03a1cca8370bfb52033157915416c87d63
ad****@an*********.com <admins@andersenlab.com>
2026-09-06 12:44:08 +0000

    genspark auto-backup

--- dist/index.js
+++ dist/index.js
@@ -5656,7 +5656,7 @@ $.use("/api/*", Le()), $.use("*", async (e, t) => {
 	let t = e.req.param("id"), n = await I.prepare("SELECT * FROM customers WHERE id = ?").bind(t).first();
 	return n ? e.json({ customer: n }) : e.json({ error: "Not found" }, 404);
 });
-var at = "b530269";
+var at = "f015f80";
 $.use("*", async (e, t) => {
 	let n = e.req.path;
 	if (!(n.endsWith(".html") && n.startsWith("/portals/"))) {
